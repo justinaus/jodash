f697be006b634d27b3a9c5c963616ee1438f81e2
Bonkook Koo <djmimi@nate.com>
2019-12-14 12:56:29 +0900

    Fix reduce init value ([] as string[])

--- src/url.ts
+++ src/url.ts
@@ -2,6 +2,6 @@ export function makeQueryStringByObject ( obj: any ) {
   let str = Object.keys(obj).reduce( (arr, k) => {
     arr.push(k + '=' + encodeURIComponent(obj[k]));
     return arr;
-  }, []).join('&');
+  }, [] as string[]).join('&');
   return str ? '?' + str : str;
 };
